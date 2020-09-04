e76103e926c348d3a8efabd8fdc8eb41c450d74d
wuushuu <laura.c.koch@gmail.com>
2020-09-04 15:35:46 +0200

    completed class JWTAuthenticationFilter

--- src/main/java/ch/zli/m223/punchclock/security/JWTAuthenticationFilter.java
+++ src/main/java/ch/zli/m223/punchclock/security/JWTAuthenticationFilter.java
@@ -1,5 +1,54 @@
 package ch.zli.m223.punchclock.security;
 
-public class JWTAuthenticationFilter extends EmailPasswordAuthenticationFilter{
+import ch.zli.m223.punchclock.domain.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Date;
+import java.util.ArrayList;
+
+public class JWTAuthenticationFilter extends EmailPasswordAuthenticationFilter{
+    private AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req,
+                                                HttpServletResponse res) throws AuthenticationException {
+        try {
+            User creds = new ObjectMapper()
+                    .readValue(req.getInputStream(), User.class);
+
+            return authenticationManager.authenticate(
+                    new EmailPasswordAuthenticationToken(
+                            creds.getEmail(),
+                            creds.getPassword(),
+                            new ArrayList<>())
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req,
+                                            HttpServletResponse res,
+                                            FilterChain chain,
+                                            Authentication auth) throws IOException, ServletException {
+
+        String token = JWT.create()
+                .withSubject(((User) auth.getPrincipal()).getEmail())
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(HMAC512(SECRET.getBytes()));
+        res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
+    }
 }
