b764fb32a31257fd2f2dee22d93d0dd5d0158067
wuushuu <laura.c.koch@gmail.com>
2020-09-04 16:17:31 +0200

    New Security Files

--- src/main/java/ch/zli/m223/punchclock/domain/User.java
+++ src/main/java/ch/zli/m223/punchclock/domain/User.java
@@ -13,6 +13,11 @@ public class User {
     private String email;
     private String password;
 
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
     //getter and setter
 
     public Long getId() {
