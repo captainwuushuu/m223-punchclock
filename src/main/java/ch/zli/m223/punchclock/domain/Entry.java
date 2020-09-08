792c204e55feccb3c24c58e0822d4a74855d17c1
wuushuu <laura.c.koch@gmail.com>
2020-09-08 21:33:49 +0200

    new class Category

--- src/main/java/ch/zli/m223/punchclock/domain/Entry.java
+++ src/main/java/ch/zli/m223/punchclock/domain/Entry.java
@@ -27,6 +27,9 @@ public class Entry {
     @Column(nullable = false)
     private LocalDateTime checkOut;
 
+    @Column(nullable = false)
+    private Category category;
+
     public Long getId() {
         return id;
     }
@@ -49,5 +52,13 @@ public class Entry {
 
     public void setCheckOut(LocalDateTime checkOut) {
         this.checkOut = checkOut;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
     }
 }
